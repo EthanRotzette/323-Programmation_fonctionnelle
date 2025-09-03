1ea86559e7b2603d25c48235229983a1856d738f
EthanRotzette <167179837+EthanRotzette@users.noreply.github.com>
2025-09-03 10:06:54 +0200

    feat(Exercice B fait)

--- exos_fait/Words/Words/Words/Program.cs
+++ exos_fait/Words/Words/Words/Program.cs
@@ -10,11 +10,13 @@ namespace Words
     {
         static void Main(string[] args)
         {
+            Action<string> writeToConsole = word => { Console.WriteLine(word); };
+            /// Exercice A ///
+            /*
             string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
 
             Func<string, bool> NoXandMoreThanFour = (word) => !word.Contains("x") && word.Length > 4 && word.Length == Math.Round(words.Average(w => w.Length), 0);
             //Func<string, bool> MoreThanFour = word => word.Length > 4;
-            Action<string> writeToConsole = word => { Console.WriteLine(word); };
             
             words = words.Where(NoXandMoreThanFour).ToArray();
             //string[] wordsWhithoutXandMoreThanFour = wordsWhithoutX.Where(MoreThanFour).ToArray();
@@ -25,7 +27,13 @@ namespace Words
             Array.Reverse(words);
             words.ToList().ForEach(message => writeToConsole(message));
             Console.ReadLine();
-            
+            */
+            /// Exercice B ///
+            string[] words = { "whatThe!!!", "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune", "My kingdom for a horse !", "Ooops I did it again" };
+
+            words = words.Skip(1).Reverse().Skip(2).Reverse().ToArray();
+            words.ToList().ForEach(message => writeToConsole(message));
+            Console.ReadLine();
         }
     }
 }
