f606a4e8d9c153c9199665ab525e4de3ed05e24d
Ethan Rotzetter <167179837+EthanRotzette@users.noreply.github.com>
2025-09-17 13:09:55 +0200

    feat(Début exercice mib-reduce)

---
+++ exos_fait/mib-reduce/mib-reduce/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mib_reduce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Pommes", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+            };
+
+            var NumberGroseilles = products.Where(p => p.ProductName == "Groseilles").Select(p => p.Quantity).Sum();
+            Console.WriteLine("Nombre de groseilles : " + NumberGroseilles);
+
+//            var ChiffreAffaire = products.GroupBy(p => p.Producer).ToList().ForEach(group=>group.Aggregate(p=>p.Quantity*p.PricePerUnit));
+  //          ChiffreAffaire.ForEach(c => Console.WriteLine("Chiffre d'affaire : " + c));
+            Console.ReadLine();
+
+        }
+        class Product
+        {
+            public int Location { get; set; }
+            public string Producer { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public string Unit { get; set; }
+            public double PricePerUnit { get; set; }
+        }
+    }
+}
