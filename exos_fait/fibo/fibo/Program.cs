982034ed6416c8b340a55c0025b2329b41cfac85
Ethan Rotzetter <167179837+EthanRotzette@users.noreply.github.com>
2025-10-29 10:49:34 +0100

    feat(exercice fibonacci)

---
+++ exos_fait/fibo/fibo/Program.cs
@@ -0,0 +1,26 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i} : {Fibonacci(i)}");
+}
+
+for (int i = 0; i < 93; i++)
+{
+    Console.WriteLine($"{i} : {FibonacciOptimise(i)}");
+}
+
+static int Fibonacci(int n)
+{
+    if (n >= 2)
+        return Fibonacci(n - 1) + Fibonacci(n -2);
+
+    return n;
+}
+
+
+static long FibonacciOptimise(long sequence, long precedent = 0, long actuel = 1)
+{
+    if (sequence > 0 )
+        return FibonacciOptimise(sequence - 1,actuel,actuel+precedent);
+
+    return precedent;
+}
